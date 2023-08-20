135f2ad47a31187927b161175d8899ad162b8862
Zyanya Garcia <zyanyagarcia8@gmail.com>
2023-08-19 21:59:05 -0700

    create main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def main():
-
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
-
+    print('Hello World')
+    print('CS V11: Programming Fundamentals')
 
 if __name__ == '__main__':
     main()
